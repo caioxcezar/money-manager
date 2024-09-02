547ce8ca1946306f9257335f6df786aedb38f867
Caio Rezende <caiocezar50@outlook.com>
2024-09-02 13:08:34 -0300

    feat: Show month

--- src/components/total.jsx
+++ src/components/total.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { now } from "@/Utils/dates";
 import ExpenseDao from "@/dao/expense";
 import PropTypes from "prop-types";
+import { DateTime } from "luxon";
 
 const CURRENCE = "R$";
 
@@ -30,11 +31,11 @@ const Total = ({ className, period }) => {
     const expenses = await ExpenseDao.getAll(null, range);
     setValue(expenses.reduce((acc, curr) => acc + Number(curr.value), 0));
   };
-
+  const month = useMemo(() => DateTime.local().toFormat("MMMM"));
   return (
     <div className={className}>
       <spam className="text-4xl">
-        Total: {`${CURRENCE} ${value.toFixed(2)}`}
+        {`Total in ${month}: ${CURRENCE} ${value.toFixed(2)}`}
       </spam>
     </div>
   );
